bc3b8e238b4741f2ccb05d3c61efddc1c336acd3
unknown <haythamgamal6>
2024-07-02 16:39:25 +0300

    small-sidebar

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+function toggleSidebar() {
+    var sidebar = document.getElementById('sidebar'); // Replace 'sidebar' with the actual ID of your sidebar element
+    sidebar.classList.toggle("small-sidebar");
+  }
+ 
